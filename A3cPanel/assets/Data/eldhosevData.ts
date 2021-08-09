2cdfd943659302cc9770ec9f700c2ac44d63f277
VargheseEldhose <88637145+VargheseEldhose@users.noreply.github.com>
2021-08-08 20:38:47 -0400

    Add files via upload

---
+++ A3cPanel/assets/Data/eldhosevData.ts
@@ -0,0 +1,11 @@
+import { eldhosev } from '../../app/eldhosev';
+export const DETAILS: eldhosev[]= [
+{
+studentID:991588959,
+studentName:"Varghese Eldhose",
+loginName:"eldhosev",
+campus:"Davis",
+assignmentTitle:"eldhosevA3"
+
+}
+];
---
+++ A3cPanel/assets/Data/myBook.ts
@@ -0,0 +1,15 @@
+import {book} from '../../app/book';
+//data are all stored here
+export const BOOKDATA:book[]=[    
+    
+            {  author:"Kishimoto", bookName:"Naruto", genre:"Action-comedy-fantasy", year:2002, picture:"assets/Images/naruto.jpg"},
+
+            {  author:"Rudyard Kipling", bookName:"Jungle Book", genre:"fantasy", year:1992, picture:"assets/Images/JungleBook.jpg"},
+
+            {  author:"Mark Twain", bookName:"Adventures of Tom Sawyer", genre:"Fantasy-comedy", year:2003, picture:"assets/Images/ATS.jpg"},
+
+            {  author:"Toriyama", bookName:"One Punch Man", genre:"Action-comedy-parody", year:2008, picture:"assets/Images/OPM.jpg"}
+
+      
+
+    ];
